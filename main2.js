b71023f1d2d2d2015268506f3f842bfdb9012860
Minh <86398860+minhnnhob@users.noreply.github.com>
2023-02-07 09:26:54 +0700

    up img

--- main2.js
+++ main2.js
@@ -1,20 +1,20 @@
-const { Console } = require('console');
-var express = require('express')
+    const { Console } = require('console');
+    var express = require('express')
 
-var app = express()
+    var app = express()
 
-app.get('/',(req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('<html><body><p> hehehehehe</p><body><html>');
-    res.end();
-})
+    app.get('/',(req,res)=>{
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<html><body><p>is Kha Banh Time </p><img src="https://znews-photo.zingcdn.me/w660/Uploaded/pwivovlb/2019_04_02/banh.jpg"><body><html>');
+        res.end();
+    })
 
-app.get('/student',(req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('<html><body><p> is student time </p><body><html>');
-    res.end();
-})
+    app.get('/student',(req,res)=>{
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<html><body><p> is student time </p><body><html>');
+        res.end();
+    })
 
-const PORT = 7000 || process.env.PORT
-app.listen(PORT)
-console.log("hehe");
+    const PORT = 7000 || process.env.PORT
+    app.listen(PORT)
+    console.log("hehe");
