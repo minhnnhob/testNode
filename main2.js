13aa0ebb6238c8287a6a588174422075f73381f2
Minh <86398860+minhnnhob@users.noreply.github.com>
2023-02-07 10:10:10 +0700

    update Student page

--- main2.js
+++ main2.js
@@ -1,4 +1,5 @@
     const { Console } = require('console');
+    var f = require('fs')
     var express = require('express')
 
     var app = express()
@@ -8,12 +9,14 @@
         res.write('<html><body><p>is Kha Banh Time </p><img src="https://znews-photo.zingcdn.me/w660/Uploaded/pwivovlb/2019_04_02/banh.jpg"><body><html>');
         res.end();
     })
-
+    var content = f.readFileSync("page1.txt","utf-8")
     app.get('/student',(req,res)=>{
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.write('<html><body><p> is student time </p><body><html>');
+        res.write(content);
         res.end();
     })
+
+
 
     const PORT = 7000 || process.env.PORT
     app.listen(PORT)
